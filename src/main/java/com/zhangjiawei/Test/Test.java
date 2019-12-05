67416b852ef6ee477171882de2b066d0b7c04189
zhangjiawei123456 <2672366107@qq.com>
2019-12-05 08:15:10 +0800

    更新

--- src/main/java/com/zhangjiawei/Test/Test.java
+++ src/main/java/com/zhangjiawei/Test/Test.java
@@ -1,7 +1,11 @@
 package com.zhangjiawei.Test;
 
 public class Test {
+	//mian方法
 	public static void main(String[] args) {
+		//输出helloword
 		System.out.println("========helloWord=========");
+		//输出世界你好
+		System.out.println("=========世界你好===============");
 	}
 }
